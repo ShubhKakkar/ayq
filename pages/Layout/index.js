3cd99551639b73333a8e777abd66648a04fc803a
ShubhKakkar <webkoala1998@gmail.com>
2022-12-24 00:19:15 +0530

    Scroll back to top

--- pages/Layout/index.js
+++ pages/Layout/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import ScrollToTop from '../../components/ScrollToTop';
 
 const Index = ({children}) => {
   return (
     <div>
         <Header />
         {children}
+        <ScrollToTop />
         <Footer />
     </div>
   )

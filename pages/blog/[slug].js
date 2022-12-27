9f143283d48c4d36fd3e131ea45d6b88f8233f34
ShubhKakkar <webkoala1998@gmail.com>
2022-12-27 12:39:11 +0530

    Single Blog  Page bottle sz-index reduced

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -13,7 +13,7 @@ const SingleBlog = (req) => {
         <meta name="description" content={`AyQ Beverages-Blog/${slug}`} />
       </Head>
       <article
-        className="px-4 py-24 mx-auto max-w-7xl font-Roboto"
+        className="px-4 pt-24 pb-4 md:py-24 mx-auto max-w-7xl font-Roboto"
         itemid="#"
         itemscope
         itemtype="http://schema.org/BlogPosting"
@@ -28,7 +28,7 @@ const SingleBlog = (req) => {
             Development
           </p>
           <h1
-            className="mb-3 uppercase text-4xl font-bold leading-loose text-dark md:text-4xl"
+            className="mb-3 uppercase text-3xl font-bold tracking-wide text-dark md:text-4xl"
             itemprop="headline"
             title="Rise of Tailwind - A Utility First CSS Framework"
           >
@@ -71,7 +71,7 @@ const SingleBlog = (req) => {
           </a>
         </div>
 
-        <div className="w-full mx-auto text-lg font-Roboto font-medium leading-loose md:w-3/4">
+        <div className="w-full mx-auto text-md md:text-lg font-Roboto font-medium leading-loose md:w-3/4">
           <p>
             What if there is an easy way to achieve responsive UI without using
             any UI kit? Can we create new and fresh designs for every project

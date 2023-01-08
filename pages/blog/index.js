2e2c92094e63b1736a3ec73a8ae1be63edb0b1ac
ShubhKakkar <webkoala1998@gmail.com>
2023-01-08 22:57:24 +0530

    Blog issue solved

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -29,53 +29,59 @@ const Blog = ({ blog }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="AyQ Beverages-Blog" />
       </Head>
-      <div className="container md:max-w-7xl pt-32 mx-auto space-y-6 sm:space-y-12 px-4 md:px-0">
-        <Link
-          rel="noopener noreferrer"
-          href={`/blog/${blog[0]?.slug}`}
-          className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline md:flex bg-gray-100 text-dark"
-        >
-          <img
-            src={blog[0]?.thumbnail}
-            alt=""
-            className="object-cover w-full h-64 md:h-[400px] lg:col-span-7"
-          />
-          <div className="py-8 pl-4 pr-4 md:pr-8 space-y-4 lg:col-span-5">
-            <h2 className="uppercase text-lg md:text-3xl font-bold leading-snug">
-              {blog[0]?.title}
-            </h2>
-            <div className="flex-[1] mt-2">
-              <p className="text-sm font-semibold text-gray-800">
-                {blog[0]?.author}
-              </p>
-              <p className="text-xs text-orange-400 font-semibold">
-                {moment.utc(blog[0]?.createdAt).format("DD-MM-YY")}
-              </p>
-            </div>
-            <div className="text-md h-[150px] font-medium leading-snug mt-4 text-ellipsis overflow-hidden">
-              {parse(blog[0]?.description)}
+      {blog && blog.length > 0 ? (
+        <>
+          <div className="container md:max-w-7xl pt-32 mx-auto space-y-6 sm:space-y-12 px-4 md:px-0">
+            <Link
+              rel="noopener noreferrer"
+              href={`/blog/${blog[0]?.slug}`}
+              className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline md:flex bg-gray-100 text-dark"
+            >
+              <img
+                src={blog[0]?.thumbnail}
+                alt=""
+                className="object-cover w-full h-64 md:h-[400px] lg:col-span-7"
+              />
+              <div className="py-8 pl-4 pr-4 md:pr-8 space-y-4 lg:col-span-5">
+                <h2 className="uppercase text-lg md:text-3xl font-bold leading-snug">
+                  {blog[0]?.title}
+                </h2>
+                <div className="flex-[1] mt-2">
+                  <p className="text-sm font-semibold text-gray-800">
+                    {blog[0]?.author}
+                  </p>
+                  <p className="text-xs text-orange-400 font-semibold">
+                    {moment.utc(blog[0]?.createdAt).format("DD-MM-YY")}
+                  </p>
+                </div>
+                <div className="text-md h-[150px] font-medium leading-snug mt-4 text-ellipsis overflow-hidden">
+                  {parse(blog[0]?.description)}
+                </div>
+              </div>
+            </Link>
+            {blog?.length > 1 && (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                {blog?.slice(1).map((post) => {
+                  return <Card key={post._id} data={post} />;
+                })}
+              </div>
+            )}
+            <div className="flex justify-center">
+              <button
+                type="button"
+                className="bg-orange-400 hover:bg-orange-500 ease-in-out duration-300 px-6 py-3 font-bold text-sm tracking-widest text-white uppercase mt-12"
+              >
+                Load more posts
+              </button>
             </div>
           </div>
-        </Link>
-        {blog?.length > 1 && (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            {blog?.slice(1).map((post) => {
-              return <Card key={post._id} data={post} />;
-            })}
-          </div>
-        )}
-        <div className="flex justify-center">
-          <button
-            type="button"
-            className="bg-orange-400 hover:bg-orange-500 ease-in-out duration-300 px-6 py-3 font-bold text-sm tracking-widest text-white uppercase mt-12"
-          >
-            Load more posts
-          </button>
-        </div>
-      </div>
-      <section className="mx-auto pt-12 md:pt-24">
-        <NewsLetter />
-      </section>
+          <section className="mx-auto pt-12 md:pt-24">
+            <NewsLetter />
+          </section>
+        </>
+      ) : (
+        <></>
+      )}
     </section>
   );
 };
